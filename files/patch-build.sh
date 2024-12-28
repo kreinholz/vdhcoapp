b83145ce9f1da4f89e78babeb8749934c02c37d4
Kevin Reinholz <kreinholz@gmail.com>
2024-12-28 08:31:42 -0800

    Simplified patch-build.sh since we're not packaging for FreeBSD

--- files/patch-build.sh
+++ files/patch-build.sh
@@ -6,17 +6,18 @@
  
  set -euo pipefail
  cd $(dirname $0)/
-@@ -25,6 +25,9 @@ case $host_os in
+@@ -25,8 +25,8 @@ case $host_os in
  host_arch=$(uname -m)
  
  case $host_os in
+-  Linux)
+-    host_os="linux"
 +  FreeBSD)
 +    host_os="freebsd"
-+    ;;
-   Linux)
-     host_os="linux"
      ;;
-@@ -75,6 +78,7 @@ case $target in
+   Darwin)
+     host_os="mac"
+@@ -75,6 +75,7 @@ case $target in
  done
  
  case $target in
@@ -24,7 +25,7 @@
    linux-i686 | \
    linux-aarch64 | \
    linux-x86_64 | \
-@@ -122,20 +126,6 @@ fi
+@@ -122,20 +123,6 @@ fi
    fi
  fi
  
@@ -45,7 +46,7 @@
  if [ $target_node == 10 ]; then
    if [[ $(pkg -v) != 4.4.9 ]]
    then
-@@ -143,11 +133,6 @@ fi
+@@ -143,11 +130,6 @@ fi
    fi
  fi
  
@@ -57,7 +58,7 @@
  if [ ! -d "app/node_modules" ]; then
    (cd app/ ; npm install)
  fi
-@@ -168,8 +153,6 @@ filepicker_target=filepicker-$target$exe_extension
+@@ -168,8 +150,6 @@ filepicker_target=filepicker-$target$exe_extension
  
  
  filepicker_target=filepicker-$target$exe_extension
@@ -66,7 +67,7 @@
  if [ $target_os == "win7" ]; then
    node_os="windows"
    ffmpeg_target=ffmpeg-$package_ffmpeg_build_version-windows-$target_arch
-@@ -193,6 +176,10 @@ fi
+@@ -193,6 +173,10 @@ fi
  if [ $target == "linux-x86_64" ]; then
    deb_arch="amd64"
  fi
@@ -77,7 +78,7 @@
  
  if [ $publish == 1 ]; then
    files=(
-@@ -314,7 +301,7 @@ if [ ! $skip_bundling == 1 ]; then
+@@ -314,7 +298,7 @@ if [ ! $skip_bundling == 1 ]; then
        "--define:import.meta.url=_importMetaUrl")
    fi
  
@@ -86,14 +87,12 @@
      "${opts[@]}" \
      --format=cjs \
      --bundle --platform=node \
-@@ -328,77 +315,50 @@ if [ ! $skip_bundling == 1 ]; then
-     declare -a opts=("$dist_dir/bundled.js")
+@@ -329,35 +313,21 @@ if [ ! $skip_bundling == 1 ]; then
    fi
  
--  log "Bundling Node binary with code"
+   log "Bundling Node binary with code"
 -  pkg "${opts[@]}" \
 -    --target node$target_node-$node_os-$node_arch \
-+#  log "Bundling Node binary with code"
 +  PKG_NODE_PATH=%%WRKDIR%%/.pkg-cache/v%%PKG_FETCH_VER%%/built-v%%PKG_NODE_VER%%-freebsd-%%NODE_ARCH%% npx pkg "${opts[@]}" \
 +    --target node$target_node-$node_os \
      --output $target_dist_dir/$package_binary_name$exe_extension
@@ -109,12 +108,12 @@
 -  filepicker_url=$filepicker_url_base/v$package_filepicker_build_version/$filepicker_target
 -  wget -c $filepicker_url -O $dist_dir/$filepicker_target
 -  chmod +x $dist_dir/$filepicker_target
-+  log "Could not find filepicker--please ensure filepicker_$target exists in $dist_dir and try again."
++  log "Could not find filepicker--please ensure $filepicker_target exists in $dist_dir and try again."
 +  exit 1
  fi
  
  cp $dist_dir/$filepicker_target $target_dist_dir/filepicker$exe_extension
- 
+-
 -if [[ ! -d $dist_dir/$ffmpeg_target_dir ]]; then
 -  log "Retrieving ffmpeg"
 -  ffmpeg_url_base="https://github.com/aclap-dev/ffmpeg-static-builder/releases/download/"
@@ -127,110 +126,6 @@
 -cp $dist_dir/$ffmpeg_target_dir/ffmpeg$exe_extension \
 -  $dist_dir/$ffmpeg_target_dir/ffprobe$exe_extension \
 -  $target_dist_dir/
--
+ 
  if [ ! $skip_packaging == 1 ]; then
  
-   log "Packaging v$meta_version for $target"
- 
-   # ===============================================
--  # LINUX
-+  # FREEBSD
-   # ===============================================
--  if [ $target_os == "linux" ]; then
--    mkdir -p $target_dist_dir/deb/opt/$package_binary_name
--    mkdir -p $target_dist_dir/deb/DEBIAN
-+  if [ $target_os == "freebsd" ]; then
-+    mkdir -p $target_dist_dir
-     # --------------------------------
-     # Variation: No ffmpeg shipped
-     # --------------------------------
-     cp LICENSE.txt README.md app/node_modules/open/xdg-open \
--      $target_dist_dir/filepicker \
-+      $target_dist_dir/$filepicker_target \
-       $target_dist_dir/$package_binary_name \
--      $target_dist_dir/deb/opt/$package_binary_name
- 
--    yq ".package.deb" ./config.toml -o yaml | \
--      yq e ".package = \"${meta_id}.noffmpeg\"" |\
--      yq e ".conflicts = \"${meta_id}\"" |\
--      yq e ".description = \"${meta_description} (with system ffmpeg)\"" |\
--      yq e ".architecture = \"${deb_arch}\"" |\
--      yq e ".depends = \"ffmpeg\"" |\
--      yq e ".version = \"${meta_version}\"" > $target_dist_dir/deb/DEBIAN/control
-+    npx ejs -f $target_dist_dir/config.json ./assets/linux/prerm.ejs \
-+      > $target_dist_dir//prerm
-+    chmod +x $target_dist_dir//prerm
- 
--    ejs -f $target_dist_dir/config.json ./assets/linux/prerm.ejs \
--      > $target_dist_dir/deb/DEBIAN/prerm
--    chmod +x $target_dist_dir/deb/DEBIAN/prerm
-+    npx ejs -f $target_dist_dir/config.json ./assets/linux/postinst.ejs \
-+      > $target_dist_dir/postinst
-+    chmod +x $target_dist_dir/postinst
- 
--    ejs -f $target_dist_dir/config.json ./assets/linux/postinst.ejs \
--      > $target_dist_dir/deb/DEBIAN/postinst
--    chmod +x $target_dist_dir/deb/DEBIAN/postinst
--
--    log "Building noffmpeg.deb file"
--    dpkg-deb --build $target_dist_dir/deb $target_dist_dir/$out_noffmpeg_deb_file
--
-     rm -rf $target_dist_dir/$package_binary_name-$meta_version
-     mkdir $target_dist_dir/$package_binary_name-$meta_version
--    cp $target_dist_dir/deb/opt/$package_binary_name/* \
-+    cp $target_dist_dir/$package_binary_name/* \
-       $target_dist_dir/$package_binary_name-$meta_version
-     log "Building .tar.bz2 file"
-     tar_extra=""
-@@ -407,51 +367,7 @@ if [ ! $skip_packaging == 1 ]; then
-     fi
-     (cd $target_dist_dir && tar -cvjS $tar_extra -f $out_noffmpeg_bz2_file $package_binary_name-$meta_version)
- 
--    # --------------------------------
--    # Variation: ffmpeg binary shipped
--    # --------------------------------
--    rm -rf $target_dist_dir/deb
--    mkdir -p $target_dist_dir/deb/opt/$package_binary_name
--    mkdir -p $target_dist_dir/deb/DEBIAN
--
--    cp LICENSE.txt README.md app/node_modules/open/xdg-open \
--      $target_dist_dir/$package_binary_name \
--      $target_dist_dir/filepicker \
--      $target_dist_dir/ffmpeg \
--      $target_dist_dir/ffprobe \
--      $target_dist_dir/deb/opt/$package_binary_name
--
--    yq ".package.deb" ./config.toml -o yaml | \
--      yq e ".package = \"${meta_id}\"" |\
--      yq e ".conflicts = \"${meta_id}.noffmpeg\"" |\
--      yq e ".description = \"${meta_description} (with builtin ffmpeg.)\"" |\
--      yq e ".architecture = \"${deb_arch}\"" |\
--      yq e ".version = \"${meta_version}\"" > $target_dist_dir/deb/DEBIAN/control
--
--    ejs -f $target_dist_dir/config.json ./assets/linux/prerm.ejs \
--      > $target_dist_dir/deb/DEBIAN/prerm
--    chmod +x $target_dist_dir/deb/DEBIAN/prerm
--
--    ejs -f $target_dist_dir/config.json ./assets/linux/postinst.ejs \
--      > $target_dist_dir/deb/DEBIAN/postinst
--    chmod +x $target_dist_dir/deb/DEBIAN/postinst
--
--    log "Building .deb file"
--    dpkg-deb --build $target_dist_dir/deb $target_dist_dir/$out_deb_file
--
-     rm -rf $target_dist_dir/$package_binary_name-$meta_version
--    mkdir $target_dist_dir/$package_binary_name-$meta_version
--    cp $target_dist_dir/deb/opt/$package_binary_name/* \
--      $target_dist_dir/$package_binary_name-$meta_version
--    log "Building .tar.bz2 file"
--    tar_extra=""
--    if [ $host_os == "mac" ]; then
--      tar_extra="--no-xattrs --no-mac-metadata"
--    fi
--    (cd $target_dist_dir && tar -cvjS $tar_extra -f $out_bz2_file $package_binary_name-$meta_version)
--
--    rm -rf $target_dist_dir/$package_binary_name-$meta_version
--    rm -rf $target_dist_dir/deb
-   fi
- 
-   # ===============================================
