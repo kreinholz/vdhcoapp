3c1edc2bc25619cc05a0110e3e6ab17d50998033
Kevin Reinholz <kreinholz@gmail.com>
2025-01-23 17:03:54 -0800

    Disabled check for Flatpak re browser registration install and uninstall scripts, since it doesn't exist on FreeBSD

--- files/patch-app_src_native-autoinstall.js
+++ files/patch-app_src_native-autoinstall.js
@@ -1,11 +1,15 @@
---- app/src/native-autoinstall.js.orig	2024-12-26 18:09:51 UTC
+--- app/src/native-autoinstall.js.orig	2025-01-23 19:43:10 UTC
 +++ app/src/native-autoinstall.js
-@@ -159,7 +159,7 @@ async function install_uninstall(uninstall = false) {
+@@ -159,11 +159,8 @@ async function install_uninstall(uninstall = false) {
    if (platform == "darwin") {
      let mode = GetMode();
      SetupFiles("mac", mode, uninstall);
 -  } else if (platform == "linux") {
 +  } else if (platform == "freebsd") {
      let mode = GetMode();
-     if (mode == "user") {
-       await PrepareFlatpak();
+-    if (mode == "user") {
+-      await PrepareFlatpak();
+-    }
+     SetupFiles("linux", mode, uninstall);
+   } else {
+     DisplayMessage("Installation from command line not supported on " + os.platform());
