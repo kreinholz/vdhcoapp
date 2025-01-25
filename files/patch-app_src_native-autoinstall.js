b8e1e7b806f975a2ba67b8b6f723cd8380d9189e
Kevin Reinholz <kreinholz@gmail.com>
2025-01-24 16:35:57 -0800

    Don't create directories to install browser manifests if they don't exist

--- files/patch-app_src_native-autoinstall.js
+++ files/patch-app_src_native-autoinstall.js
@@ -1,6 +1,18 @@
---- app/src/native-autoinstall.js.orig	2025-01-23 19:43:10 UTC
+--- app/src/native-autoinstall.js.orig	2025-01-24 16:16:23 UTC
 +++ app/src/native-autoinstall.js
-@@ -159,11 +159,8 @@ async function install_uninstall(uninstall = false) {
+@@ -116,11 +116,6 @@ async function SetupFiles(platform, mode, uninstall) {
+       try {
+         console.log(`Writing ${op.path}`);
+         let dir = path.dirname(op.path);
+-        try {
+-          await fs.mkdir(dir, { recursive: true });
+-        } catch (_) {
+-          // With node 10, this fails if directory exists.
+-        }
+         const data = new Uint8Array(Buffer.from(op.content));
+         await fs.writeFile(op.path, data);
+       } catch (err) {
+@@ -159,11 +154,8 @@ async function install_uninstall(uninstall = false) {
    if (platform == "darwin") {
      let mode = GetMode();
      SetupFiles("mac", mode, uninstall);
